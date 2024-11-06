07366cfcda43a4d260b7d5f3516354c5b1072783
Mohit722 <91499522+Mohit722@users.noreply.github.com>
2024-11-06 22:57:35 +0530

    Update getimagescan.sh

--- getimagescan.sh
+++ getimagescan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Input validation
+# Validate input parameters
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <ecr_repo> <image_tag> <region>"
     exit 1
@@ -10,44 +10,25 @@ ECR_REPO=$1
 IMAGE_TAG=$2
 REGION=$3
 SCAN_STATUS=1
-RETRY_COUNT=0
-MAX_RETRIES=10
-RETRY_INTERVAL=5  # Retry interval in seconds
 
-# Function to log messages with timestamps
-log_message() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
-}
-
-# Wait for the image scan to complete
-until [ "$SCAN_STATUS" -eq "0" ] || [ "$RETRY_COUNT" -ge "$MAX_RETRIES" ]; do
-    log_message "Waiting for ECR scan to complete (Attempt $((RETRY_COUNT+1)) of $MAX_RETRIES)..."
+# Wait for the scan to complete
+until [ "$SCAN_STATUS" -eq "0" ]; do
+    echo "Waiting for scan to complete..."
     aws ecr wait image-scan-complete --region "$REGION" --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG"
     SCAN_STATUS=$?
-    if [ "$SCAN_STATUS" -ne "0" ]; then
-        log_message "Image scan not complete. Status: $SCAN_STATUS"
-    fi
-    sleep $RETRY_INTERVAL
-    ((RETRY_COUNT++))
+    sleep 5
 done
 
-if [ "$RETRY_COUNT" -ge "$MAX_RETRIES" ]; then
-    log_message "ERROR: Image scan did not complete within the expected time."
-    exit 1
-fi
-
-# Get the scan findings and parse results
+# Get the scan findings
 SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --region "$REGION" --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG")
 CRITICAL=$(echo "$SCAN_FINDINGS" | jq '.imageScanFindings.findingSeverityCounts.CRITICAL')
 HIGH=$(echo "$SCAN_FINDINGS" | jq '.imageScanFindings.findingSeverityCounts.HIGH')
 
-# Assess vulnerabilities
-if [ "$CRITICAL" != "null" ] && [ "$CRITICAL" -gt "0" ]; then
-    log_message "CRITICAL vulnerabilities found. Exiting."
+# Check for vulnerabilities
+if { [ "$CRITICAL" != "null" ] || [ "$HIGH" != "null" ]; } && [ "$HIGH" -gt "15" ]; then
+    echo "Docker image contains vulnerabilities"
     exit 1
-elif [ "$HIGH" != "null" ] && [ "$HIGH" -gt "15" ]; then
-    log_message "HIGH vulnerabilities exceed threshold. Exiting."
-    exit 2
-else
-    log_message "INFO: No major vulnerabilities found."
 fi
+
+echo "INFO: No Vulnerabilities found"
+
