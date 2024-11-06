3b1ea939be3f73927730609d4330246a1e15257f
Mohit722 <91499522+Mohit722@users.noreply.github.com>
2024-11-07 00:03:51 +0530

    Update getimagescan.sh

--- getimagescan.sh
+++ getimagescan.sh
@@ -11,41 +11,25 @@ IMAGE_TAG=$2
 REGION=$3
 SCAN_STATUS=1
 
-# Check if image scan exists before waiting
-IMAGE_SCAN_STATUS=$(aws ecr describe-image-scan-findings --region "$REGION" --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG" 2>/dev/null)
-
-if [ -z "$IMAGE_SCAN_STATUS" ]; then
-    echo "No image scan found. Triggering the scan..."
-    aws ecr start-image-scan --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG" --region "$REGION"
-fi
-
-# Timeout after 10 minutes if scan is still not complete
-TIMEOUT=600  # 10 minutes timeout
-ELAPSED_TIME=0
-until [ "$SCAN_STATUS" -eq "0" ] || [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; do
+# Wait for the scan to complete
+until [ "$SCAN_STATUS" -eq "0" ]; do
     echo "Waiting for scan to complete..."
     aws ecr wait image-scan-complete --region "$REGION" --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG"
     SCAN_STATUS=$?
     sleep 5
-    ELAPSED_TIME=$((ELAPSED_TIME + 5))
 done
-
-if [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; then
-    echo "Timeout reached while waiting for scan completion."
-    exit 1
-fi
 
 # Get the scan findings
 SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --region "$REGION" --repository-name "$ECR_REPO" --image-id imageTag="$IMAGE_TAG")
-
-# Parse critical and high severity findings
-CRITICAL=$(echo "$SCAN_FINDINGS" | jq -r '.imageScanFindings.findingSeverityCounts.CRITICAL // 0')
-HIGH=$(echo "$SCAN_FINDINGS" | jq -r '.imageScanFindings.findingSeverityCounts.HIGH // 0')
+CRITICAL=$(echo "$SCAN_FINDINGS" | jq '.imageScanFindings.findingSeverityCounts.CRITICAL')
+HIGH=$(echo "$SCAN_FINDINGS" | jq '.imageScanFindings.findingSeverityCounts.HIGH')
 
 # Check for vulnerabilities
-if [ "$HIGH" -gt "20" ] || [ "$CRITICAL" -gt "0" ]; then
+if { [ "$CRITICAL" != "null" ] || [ "$HIGH" != "null" ]; } && [ "$HIGH" -gt "15" ]; then
     echo "Docker image contains vulnerabilities"
     exit 1
 fi
 
-echo "INFO: No vulnerabilities found"
+echo "INFO: No Vulnerabilities found"
+
+
